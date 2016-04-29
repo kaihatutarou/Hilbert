dc069856df52576df913dc28c6904b06627b698b
tarou <kaihatutarou2@gmail.com>
2016-04-29 16:00:05 +0900

    life cycle

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/HilbertView.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/HilbertView.java
@@ -59,4 +59,6 @@ public class HilbertView extends View {
         invalidate();
     }
 
+
+
 }
--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -2,6 +2,7 @@ package jp.ac.titech.itpro.sdl.hilbert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,9 +12,14 @@ public class MainActivity extends AppCompatActivity {
     private final static int MAX_N = 9;
     private int n = 1;
 
+    private static final String TAG = "MainActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreateだよ〜");
+
         setContentView(R.layout.activity_main);
         levelView = (TextView)findViewById(R.id.level_view);
         hilbertView = (HilbertView)findViewById(R.id.hilbert_view);
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG,"onResumeだよ〜");
         display();
     }
 
@@ -39,5 +46,29 @@ public class MainActivity extends AppCompatActivity {
     private void display() {
         levelView.setText(getString(R.string.level_format, n));
         hilbertView.setLevel(n);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroyだよ〜");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestartだよ〜");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPauseだよ〜");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStopだよ〜");
     }
 }
